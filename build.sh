d103e07a61da6927fcb877da480219088e2843a0
Andres Montalban <amontalban@globalitss.com>
2015-09-17 11:31:12 -0300

    [FIX] Add the --basetgz parameter to the update command

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ for RELEASE in `echo wheezy`; do
   --keyring=/etc/apt/trusted.gpg.d/debian-archive-${RELEASE}-stable.gpg \
   --mirror http://httpredir.debian.org/debian/
 
-  pbuilder update
+  pbuilder --basetgz /var/cache/pbuilder/debian-${RELEASE}.tgz update
 
   cd ${ROOT_DIR}/packages/${RELEASE}
   for PACKAGE in `ls -1d */ | cut -f1 -d'/'`; do
