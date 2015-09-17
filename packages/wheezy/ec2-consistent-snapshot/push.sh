f14fcef493526eaf46c751690dc8b16db4f23e20
Andres Montalban <amontalban@globalitss.com>
2015-09-17 14:53:04 -0300

    [MOD] Trying the --yes push option to see if overwrites the package

--- packages/wheezy/ec2-consistent-snapshot/push.sh
+++ packages/wheezy/ec2-consistent-snapshot/push.sh
@@ -10,12 +10,6 @@ mkdir -p ~/build/debs/debian/${RELEASE}/${PACKAGE}
 
 cp -Rp *.deb ~/build/debs/debian/${RELEASE}/${PACKAGE}
 
-# Remove existant packages
-for deb in `ls -1 ~/build/debs/debian/${RELEASE}/${PACKAGE}/*.deb`; do
-  FILE=`${BASENAME} $deb`
-  package_cloud yank ${REPOSITORY}/debian/${RELEASE} ${FILE}
-done
-
 # Push new packages
-package_cloud push ${REPOSITORY}/debian/${RELEASE} \
+package_cloud --yes --verbose push ${REPOSITORY}/debian/${RELEASE} \
 ~/build/debs/debian/${RELEASE}/${PACKAGE}/*.deb
