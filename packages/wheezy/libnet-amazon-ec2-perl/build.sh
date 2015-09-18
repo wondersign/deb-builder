059b09c4d6dc139a092ab6d559435ffaa91085ed
Andres Montalban <amontalban@globalitss.com>
2015-09-18 00:32:50 -0300

    [ADD] Add libnet-amazon-ec2-perl package

---
+++ packages/wheezy/libnet-amazon-ec2-perl/build.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+set -xe
+
+RELEASE=$1
+PACKAGE="libnet-amazon-ec2-perl"
+VERSION="0.29"
+
+git clone https://github.com/amontalban/${PACKAGE}.git
+cd ${PACKAGE}
+git checkout tags/debian/${VERSION}
+pdebuild --use-pdebuild-internal -- \
+--basetgz /var/cache/pbuilder/debian-${RELEASE}.tgz
