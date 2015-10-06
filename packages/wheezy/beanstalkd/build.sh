1826b81a3de86b28ea62cd0d51fd6e95b7ecf270
Andres Montalban <amontalban@globalitss.com>
2015-10-05 22:51:39 -0300

    [ADD] Generate upstream tarball

--- packages/wheezy/beanstalkd/build.sh
+++ packages/wheezy/beanstalkd/build.sh
@@ -3,8 +3,13 @@ set -xe
 
 RELEASE=$1
 PACKAGE="beanstalkd"
+VERSION="1.10"
+REVISION="1"
 
 git clone https://github.com/amontalban/${PACKAGE}.git
 cd ${PACKAGE}
+git archive --format tar.gz --prefix=beanstalkd-${VERSION}/ \
+--output ../beanstalkd_${VERSION}.orig.tar.gz upstream/${VERSION}
+git checkout tags/debian/${VERSION}-${REVISION}
 pdebuild --use-pdebuild-internal -- \
 --basetgz /var/cache/pbuilder/debian-${RELEASE}.tgz
