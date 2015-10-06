29c43a1e0303cb47da6bdcea3488c82e8b9345e2
Andres Montalban <amontalban@globalitss.com>
2015-10-05 22:05:04 -0300

    [ADD] Add beanstalkd package

---
+++ packages/wheezy/beanstalkd/build.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+set -xe
+
+RELEASE=$1
+PACKAGE="beanstalkd"
+
+git clone https://github.com/amontalban/${PACKAGE}.git
+cd ${PACKAGE}
+pdebuild --use-pdebuild-internal -- \
+--basetgz /var/cache/pbuilder/debian-${RELEASE}.tgz
