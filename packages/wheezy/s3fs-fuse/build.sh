f86bbe163d256a751df4d32701ad714dafd63b02
Andres Montalban <amontalban@globalitss.com>
2015-10-09 10:03:19 -0300

    [ADD] Add package s3fs-fuse

---
+++ packages/wheezy/s3fs-fuse/build.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -xe
+
+RELEASE=$1
+PACKAGE="s3fs-fuse"
+VERSION="1.78"
+REVISION="1"
+
+git clone https://github.com/amontalban/${PACKAGE}.git
+cd ${PACKAGE}
+git archive --format tar.gz --prefix=s3fs-fuse-${VERSION}/ \
+--output ../s3fs-fuse_${VERSION}.orig.tar.gz master
+git checkout debian
+pdebuild --use-pdebuild-internal -- \
+--basetgz /var/cache/pbuilder/debian-${RELEASE}.tgz
---
+++ packages/wheezy/s3fs-fuse/push.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+set -xe
+
+RELEASE=$1
+PACKAGE="s3fs-fuse"
+REPOSITORY="amontalban/${PACKAGE}"
+BASENAME=$(which basename)
+CURL=$(which curl)
+
+mkdir -p ~/build/debs/debian/${RELEASE}/${PACKAGE}
+
+cp -Rp *.deb ~/build/debs/debian/${RELEASE}/${PACKAGE}
+
+for deb in `ls -1 ~/build/debs/debian/${RELEASE}/${PACKAGE}/*.deb`; do
+  FILE=`${BASENAME} $deb`
+  URL="https://packagecloud.io/${REPOSITORY}/packages/debian/${RELEASE}/${FILE}/download"
+  PACKAGE_EXISTS=`${CURL} -s -o /dev/null -w "%{http_code}" ${URL}`
+  if [ ${PACKAGE_EXISTS} -ne '404' ]; then
+    # Package exists so we first need to delete it
+    package_cloud yank ${REPOSITORY}/debian/${RELEASE} ${FILE}
+  fi
+
+  # Push new packages
+  package_cloud push ${REPOSITORY}/debian/${RELEASE} \
+  ~/build/debs/debian/${RELEASE}/${PACKAGE}/${FILE}
+done
